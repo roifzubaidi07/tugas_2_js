0ae65c561d5a9c98b472dcea337ab64ae1f1ac69
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-09 23:01:51 +0700

    Tugas 2 JS
    Tugas ini kami kumpulkan untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var number = 50;
+var string = "Hallo";
+var boolean = true;
+var kosong1 = null;
+var kosong2;
+var array = [1, 2, 3, 4, 5];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(kosong1);
+console.log(kosong2);
+console.log(array);
